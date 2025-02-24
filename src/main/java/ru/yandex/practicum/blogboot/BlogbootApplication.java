81769002c88f64519fcd861939190cdd957a7ed5
sadopwnz <sadopwnz@yandex.ru>
2025-02-25 01:27:58 +0200

    Сохранение текущего состояния перед слиянием

---
+++ src/main/java/ru/yandex/practicum/blogboot/BlogbootApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.blogboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/practicum/blogboot/BlogbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.blogboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
